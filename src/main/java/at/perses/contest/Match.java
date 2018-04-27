9ef440b7a22cbc13a9aa11454950364ac66d0e15
Simon <pumba6@gmx.at>
2018-04-27 17:16:26 +0200

    level3 finished

--- src/main/java/at/perses/contest/Match.java
+++ src/main/java/at/perses/contest/Match.java
@@ -1,16 +1,21 @@
 package at.perses.contest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Match {
 
 
     public int matchCount = 0;
     public int firstOccurence = 0;
     public int lastOccurence = 0;
+//    public List<Integer> matchTimestamps = new ArrayList<>();
+    public int timeDiff = 0;
 
     public Match(Image image) {
         matchCount = 1;
         lastOccurence = image.timestamp;
         firstOccurence = image.timestamp;
+//        matchTimestamps.add(image.timestamp);
     }
-//    public Image image;
 }
